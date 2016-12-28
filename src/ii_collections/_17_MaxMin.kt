22435c93e65b09449a903e0a6dc31e474ea6828b
kang <kjxstar@gmail.com>
2016-12-28 21:55:11 +0800

    max

--- src/ii_collections/_17_MaxMin.kt
+++ src/ii_collections/_17_MaxMin.kt
@@ -1,5 +1,7 @@
 package ii_collections
 
+import java.util.*
+
 fun example4() {
     val max = listOf(1, 42, 4).max()
     val longestString = listOf("a", "b").maxBy { it.length }
@@ -7,10 +9,10 @@ fun example4() {
 
 fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? {
     // Return a customer whose order count is the highest among all customers
-    todoCollectionTask()
+    return customers.maxWith(Comparator { o, t -> o.orders.count().compareTo(t.orders.count()) })
 }
 
 fun Customer.getMostExpensiveOrderedProduct(): Product? {
     // Return the most expensive product which has been ordered
-    todoCollectionTask()
+    return orderedProducts.maxBy { it.price }
 }
