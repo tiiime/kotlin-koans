0d07893d1dc67b66d505b238c327a18e808e4845
kang <kjxstar@gmail.com>
2016-12-29 20:30:39 +0800

    add compare

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,6 +1,21 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
+    infix operator fun compareTo(target: MyDate): Int {
+        var res: Int = 0
+        res = year.compareTo(target.year)
+        if (res == 0) {
+            res = month.compareTo(target.month)
+            if (res == 0) {
+                return dayOfMonth.compareTo(target.dayOfMonth)
+            } else {
+                return res
+            }
+        } else {
+            return res
+        }
+    }
+}
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
 
