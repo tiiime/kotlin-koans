9a808a884918c4d781fa55ecfe685568b47ede86
kang <kjxstar@gmail.com>
2016-12-29 21:29:46 +0800

    si gao yi

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,5 +1,8 @@
 package iii_conventions
 
+import iii_conventions.TimeInterval.*
+
+
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
     infix operator fun compareTo(target: MyDate): Int {
         var res: Int = 0
@@ -15,6 +18,36 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) {
             return res
         }
     }
+
+    infix operator fun plus(value: TimeInterval): MyDate {
+
+        when (value) {
+            DAY -> {
+                return addTimeIntervals(DAY, 1)
+            }
+            WEEK -> {
+                return addTimeIntervals(DAY, 7)
+            }
+            YEAR -> {
+                return addTimeIntervals(YEAR, 1)
+            }
+        }
+    }
+
+    infix operator fun plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate {
+        val count = repeatedTimeInterval.count
+        when (repeatedTimeInterval.timeInterval) {
+            DAY -> {
+                return addTimeIntervals(DAY, count)
+            }
+            WEEK -> {
+                return addTimeIntervals(DAY, 7 * count)
+            }
+            YEAR -> {
+                return addTimeIntervals(YEAR, count)
+            }
+        }
+    }
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
@@ -22,8 +55,14 @@ operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 enum class TimeInterval {
     DAY,
     WEEK,
-    YEAR
+    YEAR;
+
+    infix operator fun times(i: Int): RepeatedTimeInterval {
+        return RepeatedTimeInterval(this, i)
+    }
 }
+
+class RepeatedTimeInterval(var timeInterval: TimeInterval, var count: Int)
 
 class DateRange(val start: MyDate, val endInclusive: MyDate) {
     infix operator fun contains(date: MyDate): Boolean {
