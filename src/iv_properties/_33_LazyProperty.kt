ed6dd87306dcd903f356245df583492a9ec5e598
kang <kjxstar@gmail.com>
2016-12-29 22:02:14 +0800

    a lazy property

--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -3,7 +3,9 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    val lazy: Int by kotlin.lazy {
+        initializer()
+    }
 }
 
 fun todoTask33(): Nothing = TODO(
